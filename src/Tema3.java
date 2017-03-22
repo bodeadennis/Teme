dbc63a64a2c66c0cdda9782bb8373a44688fc9af
bodeadennis <bodeadennis@gmail.com>
2017-03-22 12:36:58 +0200

    Tabla inmultirii

--- src/Tema3.java
+++ src/Tema3.java
@@ -1,41 +1,45 @@
 import java.util.Scanner;
-
 public class Tema3 {
-
     public static void main(String[] args) {
-        while (true) {
-        int x;
-        System.out.print("Introdu numarul lunii: ");
+
+        System.out.print("Tasteaza numarul unei luni: ");
+
+        int alege;
         Scanner in = new Scanner(System.in);
-        x = in.nextInt();
+        alege = in.nextInt();
 
-        if (x < 1 || x > 12) {
-            System.out.println("Nu sunt atatea luni intr-un an");}
-        else if (x == 1)
-            System.out.println("Ianuarie");
-        else if (x == 2)
-            System.out.println("Februarie");
-        else if (x ==3)
-            System.out.println("Martie");
-        else if (x==4)
-            System.out.println("Aprilie");
-        else if (x==5)
-            System.out.println("Mai");
-        else if (x==6)
-            System.out.println("Iunie");
-        else if (x==7)
-            System.out.println("Iulie");
-        else if (x==8)
-            System.out.println("August");
-        else if (x==9)
-            System.out.println("Septembrie");
-        else if (x==10)
-            System.out.println("Octombrie");
-        else if (x==11)
-            System.out.println("Noiembrie");
-        else if (x==12)
-            System.out.println("Decembrie");
 
+        String monthString;
+        switch (alege) {
+            case 1:  monthString = "Ianuarie";
+                break;
+            case 2:  monthString = "Februarie";
+                break;
+            case 3:  monthString = "Martie";
+                break;
+            case 4:  monthString = "Aprilie";
+                break;
+            case 5:  monthString = "Mai";
+                break;
+            case 6:  monthString = "Junie";
+                break;
+            case 7:  monthString = "Julie";
+                break;
+            case 8:  monthString = "August";
+                break;
+            case 9:  monthString = "Septembrie";
+                break;
+            case 10: monthString = "Octombrie";
+                break;
+            case 11: monthString = "Noiembrie";
+                break;
+            case 12: monthString = "Decembrie";
+                break;
+            default: monthString = "Luna invalida";
+                break;
+        }
+
+
+        System.out.println(monthString);
     }
-}
 }
