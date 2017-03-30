f83ada26184a70d617742be6b02b4248a15d88b6
bodeadennis <bodeadennis@gmail.com>
2017-03-30 22:16:29 +0300

    5. Se dau doua numere a si n. sa se afiseze numarul a la puterea n de ex a =3 b = 20 se va calcula 3*3 *3
    a = 3
    b = 5
    se va calcula 3*3*3*3*3

---
+++ src/Factorial.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String []args){
+        Scanner MyConsole = new Scanner(System.in);
+
+        System.out.print("Introduceti numarul pentru care se doreste calcularea factorialului: ");
+        int a = MyConsole.nextInt();
+        int fac = 1;
+        int nr=0;
+
+
+        for (nr=1 ;nr<=a ;nr++) {
+            fac = fac * nr;
+        }
+        System.out.println(fac);
+
+
+
+    }
+}
