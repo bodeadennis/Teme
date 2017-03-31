8470175425a93215c3270d2659c56659522497e8
bodeadennis <bodeadennis@gmail.com>
2017-03-31 11:41:42 +0300

    7. Se da un sir de numere. Sa se afiseze perechile de numere pentru care  primul numar este mai mare decat al doilea.  de ex pt 11 13 9 8 16 se va afisa   13 9 , 9 8

---
+++ src/PerechiNumere.java
@@ -0,0 +1,17 @@
+
+public class PerechiNumere {
+    public static void main(String []args){
+
+        int[] sirNr ={11,13,9,8,16};
+        for(int i=0 ; i < sirNr.length - 1 ;i++ ){
+               int x=sirNr[i];
+               int z=sirNr[i+1];
+
+         if (x>z)
+             System.out.println(x+" "+z);
+
+        }
+
+
+    }
+}
