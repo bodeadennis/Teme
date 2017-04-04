745d172506fbac6ce8380f4cd406207e917adcee
bodeadennis <bodeadennis@gmail.com>
2017-04-04 16:37:59 +0300

    01/04/2017 Exe 3. realizati o aplicatia numguess al carui pseudocod il stiti de la laboratorul 2 linux in java.                  practic aplicatia genereaza un numar aleator intre 1 si 10, apoi va cere sa introduceti un numar. daca l-ati ghicit va felicita altfel va da un hit gen mai mare sau mai mic dupa care va cere alt numar samd. la final va afiseaza dupa cate incercari ati reusit.

--- src/numguess.java
+++ src/numguess.java
@@ -5,33 +5,32 @@ public class numguess{
 
     public static void main(String[] args) {
 
-        int random, guess, attempts;
-        Scanner keyboard = new Scanner(System.in);
-        Random generator = new Random();
-        random = generator.nextInt(10) + 1;
-        attempts = 1;
 
-        System.out.println("Ma gandesc la un numar intre 0 si 10, ghiceste care este numarul.");
-        System.out.print("Incearca: ");
-        int nr=0;
+            int random, guess;
+            Scanner keyboard = new Scanner(System.in);
+            Random generator = new Random();
+            random = generator.nextInt(10) + 1;
 
 
-        do {
-            guess = keyboard.nextInt();
-            nr++;
-            if (guess > random) {
-                System.out.print("Mai mic: ");
-                attempts += 1;
-            }
+            System.out.println("Ma gandesc la un numar intre 0 si 10, ghiceste care este numarul.");
+            System.out.print("Incearca: ");
+            int nr = 0;
 
-              if(guess==random) {
-                  System.out.print("Felficitari, " +guess+ " este numarul si ");
-            }
-            if(guess < random){
-                System.out.print("Mai mare: ");
-            }
-        } while (guess != random);
-        System.out.println("ati ghicit din a-"+nr+" a incercare");
 
+            do {
+                guess = keyboard.nextInt();
+                nr++;
+                if (guess > random) {
+                    System.out.print("Mai mic: ");
+                }
+                if (guess == random) {
+                    System.out.print("Felficitari, " + guess + " este numarul si ");
+                }
+                if (guess < random) {
+                    System.out.print("Mai mare: ");
+                }
+            } while (guess != random);
+            System.out.println("ati ghicit din a-" + nr + " a incercare");
+
+        }
     }
-}
