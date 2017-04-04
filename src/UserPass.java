a8556aeba4f85745b051d3fb5ffefbf5e8760bc0
bodeadennis <bodeadennis@gmail.com>
2017-04-04 15:22:36 +0300

    ealizati o aplicatie care va cere sa introduceti un user si o parola. apoi daca userul este mihai si parola password 1 lanseaza aplicatia “notepad” (sau alta aplicatie) si se termina, altfel revine si cere o alta combinatie user si parola, si tot asa pana se nimereste combinatia buna.

---
+++ src/UserPass.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+import java.io.IOException;
+
+public class UserPass {
+
+    // db
+    static String user = "mihai";
+    static String parola = "password1";
+
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Log in  ");
+        boolean nimerit = false;
+        while (!nimerit) {
+
+            System.out.print("username: ");
+            String u = input.next();
+
+            System.out.print("password: ");
+            String p = input.next();
+
+
+            nimerit = isCombinatiaBuna(u, p);
+            if (nimerit) {
+                lansez();
+            }
+        }
+    }
+
+
+    public static void lansez() {
+        System.out.println("You are loggin");
+        try {
+            ProcessBuilder pb = new ProcessBuilder("Notepad.exe");
+            pb.start();
+        } catch (IOException e) {
+        }
+    }
+
+
+    public static boolean isCombinatiaBuna(String userTst, String parolaTst) {
+
+        // return true daca is alea din db altfel false
+        if (userTst.equals(user) & parolaTst.equals(parola)) {
+            return true;
+        }
+        return false;
+    }
+}
