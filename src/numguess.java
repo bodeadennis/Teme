d51724a0274019e4b2bba5a944ae2fa8ccc7f87c
bodeadennis <bodeadennis@gmail.com>
2017-04-04 15:56:47 +0300

    3. realizati o aplicatia numguess al carui pseudocod il stiti de la laboratorul 2 linux in java. practic aplicatia genereaza un numar aleator intre 1 si 10, apoi va cere sa introduceti un numar. daca l-ati ghicit va felicita altfel va da un hit gen mai mare sau mai mic dupa care va cere alt numar samd. la final va afiseaza dupa cate incercari ati reusit.

---
+++ src/numguess.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class numguess{
+
+    public static void main(String[] args) {
+
+        int random, guess, attempts;
+        Scanner keyboard = new Scanner(System.in);
+        Random generator = new Random();
+        random = generator.nextInt(10) + 1;
+        attempts = 1;
+
+        System.out.println("Ma gandesc la un numar intre 0 si 10, ghiceste care este numarul.");
+        System.out.print("Incearca: ");
+        int nr=0;
+
+        do {
+            guess = keyboard.nextInt();
+            nr++;
+            if (guess > random) {
+                System.out.print("Mai mic: ");
+                attempts += 1;
+            }
+
+              if(guess==random) {
+                  System.out.print("Felficitari, " +guess+ " este numarul si ");
+            }
+            if(guess < random){
+                System.out.print("Mai mare: ");
+            }
+        } while (guess != random);
+        System.out.println("ati ghicit din a-"+nr+" a incercare");
+
+    }
+}
